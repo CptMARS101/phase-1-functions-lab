28e11a351a56324fac53d36e3cfa832b0e46dd6a
Marcus Kitchen <marcusdkitchen@gmail.com>
2023-11-15 09:35:15 -0500

    function lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    let hq = 42
+let distance = Math.abs(blocks - hq);
+return distance;
+}
+function distanceFromHqInFeet(blocks) {
+    let hq = 42
+let distance = Math.abs(blocks - hq) * 264;
+return distance; 
+}
+function distanceTravelledInFeet(start, destination) {
+let distance = Math.abs(start - destination) * 264;
+return distance;
+}
+function calculatesFarePrice(start, destination) {
+    let distance = Math.abs(start - destination) * 264;
+    let fare = (distance - 400) * .02
+    if(distance <= 400) {
+        return 0}else if(distance > 400 && distance <= 2000) {return fare
+        }else if(distance > 2000 && distance <= 2500) {return 25
+        }else if(distance > 2500) {return "cannot travel that far"
+    }
+    
+}
